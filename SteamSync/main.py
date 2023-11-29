ede24caba31c090c586473400113e4686cb9a1fb
dmckinley2013 <dmckinley2013@fau.edu>
2023-11-28 23:16:54 -0500

    Issue #15
    Fixed nav bar

--- SteamSync/main.py
+++ SteamSync/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, jsonify
 import http.client
 import json
 import re
@@ -8,7 +8,9 @@ import urllib.parse
 app = Flask(__name__)
 
 RAPIDAPI_KEY = "c5a2ffe436msh751d8ed39930b58p19da33jsn81112dfec3fa"
+GAMESPOTAPI_KEY = "15db3545ca5bec59186fca6096262dfacf0c7659"
 RAPIDAPI_HOST = "steam2.p.rapidapi.com"
+GAME_SPOT_API_BASE_URL = "https://www.gamespot.com/api"
 
 
 def sanitize_input(input_str):
@@ -44,6 +46,55 @@ def get_reviews_for_app(appId, limit=40):
     return reviews_data
 
 
+@app.route("/home_with_news", methods=['GET'])
+def gamespot_articles():
+    # Define the endpoint and parameters for the Gamespot articles API
+    gamespot_api_key = "15db3545ca5bec59186fca6096262dfacf0c7659"
+    gamespot_endpoint = "https://www.gamespot.com/api/articles/"
+    params = {
+        'api_key': gamespot_api_key,
+        'format': 'json',
+        'limit': 100  # or any other parameters you need
+    }
+
+    headers = {
+        'User-Agent': 'SteamSync'  # Use your unique User-Agent here
+    }
+
+    try:
+        # Make the request to the Gamespot API
+        response = requests.get(gamespot_endpoint, params=params, headers=headers)
+        response.raise_for_status()  # Raise an HTTPError if an error occurred
+
+        # Parse the JSON response
+        articles_data = response.json()
+
+        # Check if 'results' key is present in the response
+        articles = articles_data.get('results', [])
+
+    except requests.exceptions.RequestException as e:
+        # Handle exceptions (print, log, or return an error response)
+        print(f"Error fetching Gamespot articles: {e}")
+        if response and response.status_code == 403:
+            print("Forbidden: Check your API key, rate limits, and endpoint access.")
+        articles = []
+
+    # Now, make an additional call to the image gallery API
+    # for article in articles:
+    #     image_gallery_endpoint = f"https://www.gamespot.com/api/image_galleries/?api_key={gamespot_api_key}&filter=id:{article['id']}&format=json"
+    #     image_response = requests.get(image_gallery_endpoint, headers=headers)
+    #     image_gallery_data = image_response.json()
+    #
+    #     if image_gallery_data.get('results'):
+    #         # gallery = image_gallery_data['results'][0]
+    #         # article['gallery_image'] = gallery['image']['original']
+    #         original_image_url = image_gallery_data['results'][0]['image']['original']
+    #         article['gallery_image'] = original_image_url
+
+    # Render the template with Gamespot articles
+    return render_template("home_with_news.html", articles=articles)
+
+
 @app.route("/home", methods=['GET'])  # Using the root path for homepage
 def home():
     conn = http.client.HTTPSConnection("steam-store-data.p.rapidapi.com")
@@ -69,6 +120,33 @@ def home():
                 print(f"Missing ID for item: {item.get('name', 'Unknown Item')}")
 
     return render_template("home.html", categories=categories)
+
+
+@app.route("/Discounted_games", methods=['GET'])
+def Discounted_games():
+    conn = http.client.HTTPSConnection("steam-store-data.p.rapidapi.com")
+
+    headers = {
+        'X-RapidAPI-Key': 'f9e9157472mshd29534641f6cf76p17a316jsnd7768c1ea96e',
+        'X-RapidAPI-Host': 'steam-store-data.p.rapidapi.com'
+    }
+
+    conn.request("GET", "/api/featuredcategories/", headers=headers)
+
+    res = conn.getresponse()
+    data = json.loads(res.read().decode("utf-8"))
+
+    categories = {k: v for k, v in data.items() if isinstance(v, dict) and 'items' in v}
+
+    # Adding detail URLs to each item using the game's ID
+    for category in categories.values():
+        for item in category['items']:
+            if 'id' in item:
+                item['detail_url'] = url_for('game_detail', game_id=item['id'])
+            else:
+                print(f"Missing ID for item: {item.get('name', 'Unknown Item')}")
+
+    return render_template("Discounted_games.html", categories=categories)
 
 
 @app.route("/game/<int:appid>/news", methods=['GET'])
@@ -175,5 +253,6 @@ def game_detail(game_id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
